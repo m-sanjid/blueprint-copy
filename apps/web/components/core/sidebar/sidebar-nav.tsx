ef30e3b4f9af77bc661354d99abf22a5d49ee5b0
Muhammed Sanjid <sanjid357@gmail.com>
2025-12-30 12:57:19 +0530

    added motion

--- apps/web/components/core/sidebar/sidebar-nav.tsx
+++ apps/web/components/core/sidebar/sidebar-nav.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
-import { AnimatePresence, motion } from "framer-motion"
+import { AnimatePresence, motion } from "motion/react"
 import {
   SidebarGroup,
   SidebarMenu,
